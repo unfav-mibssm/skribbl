0db356e666c9a28a065433ed1f6071bf9be7d0b4
unfav-mibssm <mibsamsaura@gmail.com>
2026-02-20 20:05:02 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase, ref, set, onValue, push, update, remove, onDisconnect, serverTimestamp, get } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
@@ -13,29 +12,19 @@ const firebaseConfig = {
     appId: "1:423970942237:web:ac3853dab889c0fe3305f4"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
 
-// Wait for auth to be ready
 export function initAuth() {
     return new Promise((resolve, reject) => {
         onAuthStateChanged(auth, (user) => {
             if (user) {
-                console.log("Already signed in:", user.uid);
                 resolve(user);
             } else {
-                // Try anonymous sign in
                 signInAnonymously(auth)
-                    .then((userCredential) => {
-                        console.log("Signed in anonymously:", userCredential.user.uid);
-                        resolve(userCredential.user);
-                    })
-                    .catch((error) => {
-                        console.error("Auth error:", error.code, error.message);
-                        reject(error);
-                    });
+                    .then((userCredential) => resolve(userCredential.user))
+                    .catch((error) => reject(error));
             }
         });
     });
